1b90d50f7f1580f137683d1b733ccb6b8319640e
jcortes <jacortesmahmud@gmail.com>
2015-03-20 15:07:51 -0500

    improves

--- js/app.js
+++ js/app.js
@@ -1,43 +1,78 @@
-angular.module('photoSearcher', ['ngMessages'])
+angular.module('photoSearcher', ['ngMessages', 'ngAnimate'])
 
 .controller('MainCtrl', ['$scope', '$http', '$sce', '$q', '$timeout', function($scope, $http, $sce, $q, $timeout){
     
     var vm = $scope;
     
-    function wait() {
+    function wait(ms) {
         return $q(function(resolve, reject){
             $timeout(function() {
                 resolve();
-            }, 1000);
+            }, ms);
         });
     }
 
     function notify() {
-        return wait().then(function() {
-            vm.tagname = null;
+        return wait(5000).then(function() {
+            vm.showMsg = false;
         });
     }
     
-    vm.searchPhotos = function(tagname){
+    vm.searchPhotos = function(tag){
         
-        var url = 'https://api.instagram.com/v1/tags/' + tagname + '/media/recent';
+        if(vm.photos) vm.photos = null;
+        if(vm.frm.$invalid){
+            vm.showMsg = false;
+            return;
+        }
+        
+        var searchMsg = 'Searching Instagram for photos tagged with \"' + tag + '\"';
+        var foundMsg = 'We found count results for \"' + tag + '\"';
+        var notFoundMsg = 'No images were found';
+        var errorMsg = 'There was an error calling the InstagramAPI';
+        var wordMsg = 'There was a problem with tag \"' + tag + '\"';
+        
+        vm.showMsg = true;
+        vm.msg = searchMsg;
+        vm.tagname = null;
+        vm.frm.$submitted = false;
+        
+        tag = tag.replace(/ /g, '_');
+        
+        var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent';
         var request = {
             client_id: '79c25bc624914d2fb102c89a94932c55',
-            callback: 'JSON_CALLBACK',
-            count: 9
+            callback: 'JSON_CALLBACK'
         };
 
-        $http({
-            method: 'JSONP',
-            url: url,
-            params: request
-        })
-        .success(function(data, status, headers, config){
-            vm.photos = data.data;
-            notify();
-        })
-        .error(function(data, status, headers, config){
-            console.log('Error getting data with http status code: ' + status);
+        wait(2000).then(function(){
+            $http({
+                method: 'JSONP',
+                url: url,
+                params: request
+            })
+            .success(function(data, status, headers, config){
+                vm.photos = data.data;
+                if(vm.photos){
+                    vm.count = vm.photos.length;
+                    if(vm.count > 0){
+                        vm.msg = foundMsg.replace(/count/g, vm.count);
+                    } else {
+                        vm.msg = notFoundMsg;
+                        notify();
+                    }
+                } else {
+                    vm.msg = wordMsg;
+                    notify();
+                }
+            })
+            .error(function(data, status, headers, config){
+                vm.msg = errorMsg;
+                notify();
+                console.log('Error getting data with http status code: ' + status);
+            });
+            
         });
+            
     };
 }]);
